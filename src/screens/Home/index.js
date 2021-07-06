ccbcc6929f9e4814cf8f2896788fcb89301f6c94
Djonata Webber <wdjonata@gmail.com>
2021-07-06 20:15:48 -0300

    update page Home

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,9 +1,91 @@
-import React from 'react';
+import React, { useMemo, useState, useEffect } from 'react';
+import { useNavigation } from '@react-navigation/native'
 
-import { Container, HeaderArea, HeaderTitle } from './styles';
+import pt from 'date-fns/locale/pt'
+import { format } from 'date-fns';
+import { FlatList } from 'react-native'
+
+import HoursDayItem from '../../components/HoursDayItem'
 import Background from '../../components/Background'
 
+import { Container,
+    HeaderArea,
+    HeaderTitle,
+
+    DateTimeArea,
+    TimeText,
+    DateText,
+
+    ButtonArea,
+    Button,
+    ButtonText,
+
+    RegisterDayArea,
+    RegisterDayTitle,
+    RegisterDayTitleArea,
+
+    RecentRegisterArea,
+    RecentRegisterTitle,
+
+} from './styles';
+
 export default () => {
+
+    const navigation = useNavigation()
+
+    const handleClickOccurrence = () => {
+        navigation.navigate('Occurrence')
+    }
+
+    const handleClickPointRegister = () => {
+        navigation.navigate('PointRegister')
+    }
+
+    const listDataDay = {date:'11/05/2021',
+        data:[
+            {key:1, time:'08:00'},
+            {key:2, time:'12:00'},
+        ]
+    }
+
+    const listDataRecent = [
+        {key:1, date:'11/05/2021', data:[
+            {key:1, time:'08:00'},
+            {key:2, time:'12:00'},
+            {key:3, time:'13:00'},
+            {key:4, time:'17:00'},
+        ] },
+        {key:2, date:'12/05/2021', data: [
+            {key:1, time:'08:00'},
+            {key:2, time:'12:00'},
+            {key:3, time:'13:00'},
+            {key:4, time:'18:00'},
+            {key:5, time:'19:00'},
+            {key:6, time:'22:00'},
+        ]},
+        {key:3, date:'13/05/2021', data: [
+            {key:1, time:'08:00'},
+            {key:2, time:'12:05'},
+            {key:3, time:'13:00'},
+            {key:4, time:'18:00'},
+        ]},
+    ]
+
+    const [date, setDateTime] = useState(new Date())
+
+    const handleDate = useMemo(
+        () => format(date, "dd 'de' MMMM 'de' yyyy", {locale: pt}),
+        [date]
+    )
+
+    const handleTime = useMemo(
+        () => format(date, "HH:mm", {locale: pt}),
+        [date]
+    )
+
+    useEffect(() => {
+        setInterval(( ) => setDateTime(new Date( )), 60000)
+    }, []);
 
     return(
 
@@ -12,6 +94,42 @@ export default () => {
                 <HeaderArea>
                     <HeaderTitle>Home</HeaderTitle>
                 </HeaderArea>
+                <DateTimeArea>
+                    <TimeText>{handleTime}</TimeText>
+                    <DateText>{handleDate}</DateText>
+                </DateTimeArea>
+                <ButtonArea>
+                    <Button>
+                        <ButtonText
+                            onPress={handleClickPointRegister}
+                        >Registra Ponto</ButtonText>
+                    </Button>
+                    <Button>
+                        <ButtonText
+                            onPress={handleClickOccurrence}
+                        >
+                        Ocorrência
+                        </ButtonText>
+                    </Button>
+
+                </ButtonArea>
+                <RegisterDayArea>
+                    <RegisterDayTitleArea>
+                        <RegisterDayTitle>Hoje</RegisterDayTitle>
+                    </RegisterDayTitleArea>
+                    <HoursDayItem data={listDataDay}/>
+                </RegisterDayArea>
+
+                <RecentRegisterArea>
+                    <RecentRegisterTitle>Últimos dias</RecentRegisterTitle>
+                    <FlatList
+                        data={listDataRecent}
+                        renderItem={({item}) => (
+                            <HoursDayItem data={item}/>
+                        )}
+                    />
+                </RecentRegisterArea>
+
             </Container>
         </Background>
     )
--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -17,3 +17,69 @@ export const HeaderTitle = styled.Text`
     font-weight: bold;
     color: #FFF;
 `;
+
+export const DateTimeArea = styled.View`
+    flex: 3;
+    align-items: center;
+    justify-content: center;
+`;
+export const ButtonArea = styled.View`
+    flex: 1;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+`;
+export const RegisterDayArea = styled.View`
+    flex: 2;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const RegisterDayTitle = styled.Text`
+    color: #FFF;
+    font-size: 16px;
+`;
+
+export const RegisterDayTitleArea = styled.View`
+    width: 100%;
+    justify-content: center;
+    padding-left: 5px;
+`;
+
+export const RecentRegisterArea = styled.View`
+    flex: 3;
+    margin-bottom: 20px;
+`;
+
+export const TimeText = styled.Text`
+    font-size: 60px;
+    font-weight: bold;
+    color: #FFF;
+`;
+
+export const DateText = styled.Text`
+    font-size: 18px;
+    color: #FFF;
+`;
+
+export const RecentRegisterTitle = styled.Text`
+    color: #FFF;
+    font-size: 16px;
+`;
+
+export const Button = styled.TouchableOpacity`
+    width: 180px;
+    height: 40px;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    background-color: #004561;
+    border-radius: 6px;
+
+`;
+
+export const ButtonText = styled.Text`
+    font-size: 14px;
+    color: #FFFFFF;
+    padding: 10px
+`;
